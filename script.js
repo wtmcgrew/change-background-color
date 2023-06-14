5f96228a145e241b715413b9bf131492d42b3446
Whitney McGrew <wt.mcgrew@gmail.com>
2023-06-13 22:39:27 -0500

    Function logic with DOM targeting option values for color

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeBackgroundColor(value) {
+	document.body.style.backgroundColor=value;
+}
+
+/*
+
+The onchange event occurs when the value of an HTML element is changed
+
+Defined function chnagedBackgroundColor and passed through value as argument from HTML document when code is ran
+
+Whatever the value is set to will change background color of page
+
+body.style adds style attribute to body element on HTMl
+
+*/
